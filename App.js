55d06093d40463f78df9e6fde1c85f6ce6bf4904
davidval99 <davidvalverde99@gmail.com>
2021-01-10 00:27:57 -0600

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        style={StyleSheet.absoluteFillObject}
+        provider={MapView.PROVIDER_GOOGLE}
+        region={{
+          latitude: 9.917415,
+          longitude: -84.034018,
+          latitudeDelta: 0.05953,
+          longitudeDelta: 0.044982,
+        }}
+      ></MapView>
+      <Text>A</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
